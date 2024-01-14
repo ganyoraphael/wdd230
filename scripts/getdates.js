98b321a13c3a3905deda70c91e66c0941b8f66bf
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2024-01-13 19:44:01 -0700

    first commit

---
+++ scripts/getdates.js
@@ -0,0 +1,14 @@
+/*
+
+// getdates.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current year
+    var currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").innerText = currentYear;
+
+    // Get the last modification date of the document
+    var lastModified = new Date(document.lastModified);
+    document.getElementById("lastModified").innerText = lastModified.toLocaleString();
+});
+*/
