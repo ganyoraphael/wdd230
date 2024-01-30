c453a37e1f9979bd402da8e606a41e0a835f881e
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2024-01-30 16:30:11 -0700

    new up

---
+++ lesson02A/scripts/script.js
@@ -0,0 +1,3 @@
+ // JavaScript for dynamic dates
+ document.getElementById("modification-date").innerText = new Date().toLocaleString();
+ document.getElementById("copyright").innerText = '\u00A9 ' + new Date().getFullYear() + ' Raphael Ganyo';
