bb4c538b992e7bd4ea499f1f023c312cc6591d2e
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2024-02-08 19:58:49 -0700

    up2

---
+++ chamber/scripts/menu.js
@@ -0,0 +1,7 @@
+// Responsive Menu
+// const nav = document.querySelector("nav");
+// const menuButton = document.querySelector("#MenuButton");
+
+// menuButton.addEventListener("click", () => {
+//     nav.classList.toggle("menuOpen");
+// });
