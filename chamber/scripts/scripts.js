d36d76487d9161f7d727858652fabd7b7313a5a1
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2024-02-09 19:22:19 -0700

    up5

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,42 +1,24 @@
-function toggleManu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
+/// Footer updates
+const date = new Date();
+const yearString = date.toLocaleDateString('en-US', {
+    year: 'numeric'
+});
+document.querySelector("#CopyrightYear").innerHTML = yearString;
+document.querySelector("#LastUpdated").innerHTML = document.lastModified;
 
-}
+/// Responsive Menu
+const nav = document.querySelector("nav");
+const menuButton = document.querySelector("#MenuButton");
 
-const x = document.getElementById('hamburgerBtn')
-x.onclick = toggleManu;
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("menuOpen");
+});
 
-let date = new Date
 
-const dateOptions = {
-    year:'2-digit',
-    month:'long',
-    day:'numeric'
+/// Dark Mode Support
+const darkModeButton = document.querySelector("#DarkModeButton");
+const body = document.querySelector("body");
 
-}
-let year = date.getFullYear()
-console.log(year)
-let years = document.querySelectorAll(".year");
-console.log(years)
-
-function displayYear(x) {
-    for(let i=0; i<x.length; i++) {
-        x[i].textContent = date.toLocaleDateString('en-US', dateOptions);
-    }
-}
-
-displayYear(years)
-
-document.querySelector("#lastmodified").textContent = document.lastModified;
-
-// Show the meet-greet message on Tue/Thu
-// Date.getDay() uses a 0 based index to return the day of the week
-var messagedate = new Date();
-console.log(messagedate.getDay())
-if (messagedate.getDay()==2 || messagedate.getDay()==4){
-    document.querySelector("#meet-greet").classList.add('active');
-} else {
-    document.querySelector("#meet-greet").style.display = "none";
-
-}
+darkModeButton.addEventListener('click', () => {
+    body.classList.toggle("darkMode");
+});
