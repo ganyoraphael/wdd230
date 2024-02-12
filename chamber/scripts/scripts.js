86fb170de8861b3ecdf15435b246bb7f43b999c8
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2024-02-12 15:35:05 -0700

    a

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -3,3 +3,16 @@ const date = new Date();
 const yearString = date.getFullYear(); // Just get the full year
 document.getElementById("CopyrightYear").textContent = yearString;
 document.getElementById("LastUpdated").textContent = document.lastModified;
+
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const hamburgerBtn = document.getElementById('hamburgerBtn');
+    const primaryNav = document.getElementById('primaryNav');
+
+    hamburgerBtn.addEventListener('click', function() {
+        primaryNav.classList.toggle('open');
+        hamburgerBtn.classList.toggle('is-active'); // Toggle a class to change the button's appearance
+    });
+});
