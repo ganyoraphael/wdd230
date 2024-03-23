5d6d1365dc1e9df4ffc2cc8570fc6bf2712fa23b
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2024-03-23 01:45:39 -0600

    44

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -1,39 +1,29 @@
+
+
 // const darkmode = document.querySelector('#dark-mode');
 // const bodyelt = document.querySelector("body");
 
 // darkmode.addEventListener('click',()=>{
-//     if (darkmode.textContent == 'DARK' ){
-//         document.documentElement.style.setProperty('--text-color', 'rgb(249, 245, 245)');        
-//         document.documentElement.style.setProperty('--background-color', 'black');    
+//     if (darkmode.textContent == '🌙' ){
+//         document.documentElement.style.setProperty('--text-color', 'rgb(205, 133, 63');        
+//         document.documentElement.style.setProperty('--background-color', 'gray');    
             
-//         bodyelt.style.backgroundColor = 'black';
-//         darkmode.textContent = 'LIGHT'
+//         bodyelt.style.backgroundColor = 'gray';
+//         darkmode.textContent = '🔆'
 //     }
 //     else{
-//         document.documentElement.style.setProperty('--text-color', 'black');        
-//         document.documentElement.style.setProperty('--background-color', 'rgb(249, 245, 245)');        
+//         document.documentElement.style.setProperty('--text-color', 'gray');        
+//         document.documentElement.style.setProperty('--background-color', 'rgb(205, 133, 63');        
         
-//         bodyelt.style.backgroundColor = 'rgb(249, 245, 245)';
-//         darkmode.textContent = 'DARK'
+//         bodyelt.style.backgroundColor = 'rgb(205, 133, 63';
+//         darkmode.textContent = '🌙'
 //     }
 // })
 
-const darkmode = document.querySelector('#dark-mode');
-const bodyelt = document.querySelector("body");
+const modeButton = document.querySelector('#color-mode');
+const darkMode = document.querySelector('body');
 
-darkmode.addEventListener('click',()=>{
-    if (darkmode.textContent == '🌙' ){
-        document.documentElement.style.setProperty('--text-color', 'rgb(205, 133, 63');        
-        document.documentElement.style.setProperty('--background-color', 'gray');    
-            
-        bodyelt.style.backgroundColor = 'gray';
-        darkmode.textContent = '🔆'
-    }
-    else{
-        document.documentElement.style.setProperty('--text-color', 'gray');        
-        document.documentElement.style.setProperty('--background-color', 'rgb(205, 133, 63');        
-        
-        bodyelt.style.backgroundColor = 'rgb(205, 133, 63';
-        darkmode.textContent = '🌙'
-    }
-})
+modeButton.addEventListener('click', () => {
+    modeButton.classList.toggle('dark-mode');
+    darkMode.classList.toggle('dark-mode');
+});
