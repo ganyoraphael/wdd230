615dfeeb765f7e35040061aa2a5c642d93f6f0e0
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2024-03-24 20:12:25 -0600

    dd

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -1,25 +1,4 @@
 
-
-// const darkmode = document.querySelector('#dark-mode');
-// const bodyelt = document.querySelector("body");
-
-// darkmode.addEventListener('click',()=>{
-//     if (darkmode.textContent == '🌙' ){
-//         document.documentElement.style.setProperty('--text-color', 'rgb(205, 133, 63');        
-//         document.documentElement.style.setProperty('--background-color', 'gray');    
-            
-//         bodyelt.style.backgroundColor = 'gray';
-//         darkmode.textContent = '🔆'
-//     }
-//     else{
-//         document.documentElement.style.setProperty('--text-color', 'gray');        
-//         document.documentElement.style.setProperty('--background-color', 'rgb(205, 133, 63');        
-        
-//         bodyelt.style.backgroundColor = 'rgb(205, 133, 63';
-//         darkmode.textContent = '🌙'
-//     }
-// })
-
 const modeButton = document.querySelector('#color-mode');
 const darkMode = document.querySelector('body');
 
