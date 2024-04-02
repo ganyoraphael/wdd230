1931855bb10ad26743e202633ed28ec76ca2079a
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2024-04-02 06:27:49 -0600

    b

--- bountiful/raph/scripts/responsiveMenu.js
+++
@@ -1,7 +0,0 @@
-const hamButton = document.querySelector('#responsive-menu');
-const navigation = document.querySelector('#navigation');
-
-hamButton.addEventListener('click', () => {
-    hamButton.classList.toggle('open');
-    navigation.classList.toggle('open');
-});
